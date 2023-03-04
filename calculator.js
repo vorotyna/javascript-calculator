d13c87350692d30c0d18c76624580e2776ef1715
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-03-04 16:32:29 -0600

    Create a button click function

--- calculator.js
+++ calculator.js
@@ -1,3 +1,8 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+let screen = document.querySelector(".screen");
+
 function buttonClick(value) {
   if (isNaN(parseInt(value))) {
     handleSymbol(value);
@@ -6,3 +11,36 @@ function buttonClick(value) {
   }
   rerender();
 }
+
+function handleSymbol(value) {
+  switch (value) {
+    case "C":
+      buffer = "0";
+      runningTotal = 0;
+      break;
+    case "←":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    case "=":
+      if (previousOperator === null) {
+        return;
+      } else {
+        handleOperation(parseInt(buffer));
+        previousOperator = null;
+        buffer += runningTotal;
+        runningTotal = 0;
+      }
+      break;
+    case "÷":
+    case "×":
+    case "-":
+    case "+":
+      handleMath(value);
+      break;
+  }
+}
+
